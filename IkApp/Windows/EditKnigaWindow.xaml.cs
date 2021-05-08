5a1bc01aa9d8993ff7638d83a76d213c50be1783
unknown <farin.octrovz@gmail.com>
2021-05-08 18:27:20 +0300

    vitalik eto tebe

--- IkApp/Windows/EditKnigaWindow.xaml.cs
+++ IkApp/Windows/EditKnigaWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IkApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,59 @@ namespace IkApp.Windows
     /// </summary>
     public partial class EditKnigaWindow : Window
     {
+        BibEntities bibEntities = new BibEntities();
+        Kniga _kniga = new Kniga();
+
         public EditKnigaWindow()
         {
             InitializeComponent();
         }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            if (ModelCheck())
+            {
+                try
+                {
+                    if (_kniga.Id == 0)
+                    {
+                        
+                        _kniga.Author = Author.Text;
+                        _kniga.Name = Name.Text;
+                        _kniga.MestoIzdaniya = Mesto.Text;
+                        _kniga.GodIzdaniya = God.Text;
+                        _kniga.RazdelSistematicheskogoKataloga = Razdel.Text;
+                        _kniga.Keyword = Keyword.Text;
+                        _kniga.NoteToEmployees = NoteToEmployees.Text;
+
+                    }
+
+                }
+                catch
+                {
+
+                }
+
+             } 
+        }
+        private bool ModelCheck()
+        {
+            StringBuilder error = new StringBuilder();
+            if (Name.Text == "")
+                error.AppendLine("Укажите название");
+            if (Razdel.Text == "")
+                error.AppendLine("Укажите раздел систематического каталога");
+
+
+            if (error.Length > 0)
+            {
+                MessageBox.Show(error.ToString());
+                return false;
+            }
+            else
+                return true;
+        }
     }
 }
+
+    
