83ec3147abe693c72f2eaee25db08a62d5869f1e
unknown <farin.octrovz@gmail.com>
2021-05-08 15:57:56 +0300

    add admin and user

--- IkApp/Windows/MenagerWindow.xaml.cs
+++ IkApp/Windows/MenagerWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IkApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,66 @@ namespace IkApp.Windows
     /// </summary>
     public partial class MenagerWindow : Window
     {
+        BibEntities bibEntities = new BibEntities();
+
         public MenagerWindow()
         {
             InitializeComponent();
+            datagridkniga.AutoGenerateColumns = false;
+            datagridkniga.ItemsSource = bibEntities.Kniga.ToList();
+            datagridbron.ItemsSource = bibEntities.Journal.ToList();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            EditKnigaWindow editKnigaWindow = new EditKnigaWindow();
+            editKnigaWindow.Show();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            if (datagridkniga.SelectedItems.Count > 0)
+            {
+                Kniga kniga = (Kniga)datagridkniga.SelectedItems[0];
+                EditKnigaWindow editKnigaWindow = new EditKnigaWindow(kniga);
+                editKnigaWindow.Show();
+            }
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+       
+
+        private void Edit2_Click(object sender, RoutedEventArgs e)
+        {
+            if (datagridbron.SelectedItems.Count > 0)
+            {
+                Journal journal = (Journal)datagridbron.SelectedItems[0];
+                EditJournalWindow editJournalWindow = new EditJournalWindow(journal);
+                editJournalWindow.Show();
+            }
+        }
+
+        private void Refresh2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit2_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
         }
     }
 }
